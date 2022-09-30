fbf93b61727660cb79cfb80b602316815b04d6a6
Felipe Santos <lipsantos013@gmail.com>
2022-09-29 22:58:02 -0300

    atualização

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+$(document).ready(function(){
+
+    //aplica uma função click dos botões comprar
+    $(".card-buy").click(function(){
+        alert("Produto Indisponível");
+    });
+
+});
+
+
+
+
 function showNavbarMobile() {
     //alterar o css da navbar
     const navbar = document.getElementById('menu');
